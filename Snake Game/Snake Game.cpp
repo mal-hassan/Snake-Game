9f4f6c5cdbeca661d3f137b5642283435f9d0031
mal-hassan <89387626+mal-hassan@users.noreply.github.com>
2024-12-19 06:03:27 +0000

    Add files via upload

---
+++ Snake Game/Snake Game.cpp
@@ -0,0 +1,64 @@
+#include <raylib.h>
+#include <iostream>
+
+using namespace std;
+
+Color green = { 173,204, 96, 255 };
+Color darkGreen = { 43,51,24,255 };
+
+int cellSize = 30;
+int cellCount = 25;
+
+class Food {
+public:
+    Vector2 position;
+    Texture2D texture;
+
+    Food() {
+        Image image = LoadImage("Graphics/food.png");
+            texture = LoadTextureFromImage(image);
+            UnloadImage(image);
+            position = GenerateRandomPos();
+    }
+
+    ~Food() {
+        UnloadTexture(texture);
+    }
+
+    void Draw() {
+
+        DrawTexture(texture, position.x * cellSize, position.y * cellSize,WHITE);
+    }
+
+    Vector2 GenerateRandomPos() {
+        float x = GetRandomValue(0, cellCount - 1);
+        float y = GetRandomValue(0, cellCount - 1);
+        return Vector2{ x,y };
+    }
+
+
+};
+
+int main()
+{
+    cout << "starting the game.." << endl;
+    InitWindow(cellSize*cellCount, cellSize * cellCount, "Retro Snake");
+    SetTargetFPS(60);
+
+    Food food = Food();
+
+    while (WindowShouldClose() == false) {
+
+        BeginDrawing();
+
+        //Drawing
+        ClearBackground(green);
+        food.Draw();
+
+        EndDrawing();
+
+    }
+
+    CloseWindow();
+    return 0;
+}
